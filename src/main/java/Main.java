6f448f80f8512fc51731096f0184dfceb515b802
ruppal <raheeluppal323@gmail.com>
2020-03-23 10:35:03 -0400

    name/occurrences

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,9 +11,33 @@ public class Main {
         return result;
     }
 
+    public Integer nmbrOfOccurrences(String string, String word){
+        int count = 0, startIndex = 0;
+
+        Pattern p = Pattern.compile(word, Pattern.CASE_INSENSITIVE+Pattern.LITERAL);
+        Matcher m = p.matcher(string);
+
+        while(m.find(startIndex)){
+            count++;
+            startIndex = m.start() + 1;
+        }
+        return count;
+    }
+
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
+
+        System.out.println("name: \t Milk \t\t seen: "+new Main().nmbrOfOccurrences(output,"milk") +" times");
+        System.out.println("============= \t \t =============");
+        System.out.println("name: \t Bread \t\t seen: "+new Main().nmbrOfOccurrences(output,"bread")+" times");
+        System.out.println("============= \t \t =============");
+        System.out.println("name: \t Cookies \t seen: "+new Main().nmbrOfOccurrences(output,"cookies")+" times");
+        System.out.println("============= \t \t =============");
+        System.out.println("name: \t Apples \t seen: "+new Main().nmbrOfOccurrences(output,"apples")+" times");
+        System.out.println("============= \t \t =============");
+
 
     }
+
+
 }
